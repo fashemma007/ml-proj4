d022a4539413a82569f3d71ff5f59138e0ab0604
fashemma007 <fasogbaemmanuel@gmail.com>
2022-07-11 09:09:04 +0100

    Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=emiwest/udaproj:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy udaproj --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/udaproj-64b88976b7-bl6m2  --address 0.0.0.0 8000:80
